4c9a98a97a1c658f235681461a246d337ea783a6
Honken Park <honken.park@gmail.com>
2024-12-09 22:54:54 +0900

    FEAT(Index): Apply AppBar
    Apply AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_mold_vault/views/index_screen.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MoldVaultApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
+  Get.put(MoldVaultController());
+
+  runApp(MoldVaultApp());
+}
+
+class MoldVaultController extends GetxController {
+  final RxBool _isDarkMode = false.obs;
+  bool get isDarkMode => _isDarkMode.value;
+  set isDarkMode(bool value) => _isDarkMode.value = value;
 }
 
 class MoldVaultApp extends StatelessWidget {
-  const MoldVaultApp({super.key});
+  MoldVaultApp({super.key});
+  final MoldVaultController moldVaultController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //   fontFamily: 'FontFamilyName',
-      //   textTheme: const TextTheme(
-      //     bodyLarge: TextStyle(fontFamily: 'FontFamilyName', fontWeight: FontWeight.w400),
-      //     bodyMedium: TextStyle(fontFamily: 'FontFamilyName', fontWeight: FontWeight.w700),
-      //     headlineLarge: TextStyle(fontFamily: 'FontFamilyNameOther', fontWeight: FontWeight.w700),
-      //   ),
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-      // ),
-      title: 'Flutter Mold Vault',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en'),
-        Locale('ko', 'KR'),
-      ],
-    );
+    return Obx(() => GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            // fontFamily: 'FontFamilyName',
+            // textTheme: const TextTheme(
+            //   bodyLarge: TextStyle(fontFamily: 'FontFamilyName', fontWeight: FontWeight.w400),
+            //   bodyMedium: TextStyle(fontFamily: 'FontFamilyName', fontWeight: FontWeight.w700),
+            //   headlineLarge: TextStyle(fontFamily: 'FontFamilyNameOther', fontWeight: FontWeight.w700),
+            // ),
+            colorScheme: moldVaultController.isDarkMode ? const ColorScheme.dark(primary: Colors.indigo) : const ColorScheme.light(primary: Colors.indigo),
+          ),
+          title: 'Flutter Mold Vault',
+          home: Scaffold(
+            appBar: AppBar(
+              title: Obx(() => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(moldVaultController.isDarkMode ? 'Set Light Mode' : 'Set Dark Mode'),
+                      Switch(
+                        value: moldVaultController.isDarkMode,
+                        onChanged: (value) {
+                          moldVaultController.isDarkMode = value;
+                        },
+                      ),
+                    ],
+                  )),
+            ),
+            body: const Center(
+              child: IndexScreen(),
+            ),
+          ),
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('ko', 'KR'),
+          ],
+        ));
   }
 }
