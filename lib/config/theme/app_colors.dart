d0b073ff963743209f1fe7c72320090bd3f52f09
Hongkeun-Park <honken.park@saig.co.kr>
2025-02-14 16:55:33 +0900

    FEAT(LottieScreen): Update AppColor

--- lib/config/theme/app_colors.dart
+++ lib/config/theme/app_colors.dart
@@ -23,16 +23,18 @@ class AppColors {
   static const Color oceanBlue = Color(0xFF354BB8);
   static const Color coolSlateBlue = Color(0xFF7986CB);
   static const Color indigoNight = Color(0xFF303F9F);
-  static const Color jejuPassDeepBlue = Color(0xFF142160);
+  static const Color jpDeepBlue = Color(0xFF142160);
 
   // Red
   static const Color burntRose = Color(0xFFB83A3A);
   static const Color charcoalRed = Color(0xFF8E3838);
   static const Color crimsonFlare = Color(0xFFB00020);
+  static const Color rebootRed = Color(0xFFE56161);
 
   // Green
   static const Color aquaMint = Color(0xFF03DAC6);
   static const Color vibrantGrass = Color(0xFF4CAF50);
+  static const Color excelGreen = Color(0xFF217346);
 
   // Yellow
   static const Color amberGlow = Color(0xFFFFA000);
